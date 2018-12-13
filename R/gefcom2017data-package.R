167e0a122c0fef087b5771b138ec64e734512a9a
Cameron Roach <camroach87@gmail.com>
2018-12-13 16:45:01 +1100

    Documentation for gefcom data set

--- R/gefcom2017data-package.R
+++ R/gefcom2017data-package.R
@@ -3,3 +3,13 @@ NULL
 
 #' @export
 magrittr::`%>%`
+
+#' GEFCom2017 data set
+#'
+#' Contains energy and temperature data for eight bottom level zones and two
+#' aggregated zones for New England.
+#'
+#' @name gefcom
+#' @docType data
+#' @keywords data
+NULL
