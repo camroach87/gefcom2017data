43737e7da0fdefcf54122b0d1855852d48283bc8
Cameron Roach <camroach87@gmail.com>
2020-02-22 12:16:31 +1100

    Update documentation and add sources

--- R/gefcom2017data-package.R
+++ R/gefcom2017data-package.R
@@ -9,6 +9,10 @@ magrittr::`%>%`
 #' Contains energy and temperature data for eight bottom level zones and two
 #' aggregated zones for New England.
 #'
+#' @source ISO New England 2017, Energy, Load, and Demand Reports, url: https://www.iso-ne.com/isoexpress/web/reports/load-and-demand/-/tree/zone-info
+#'
+#' @source U.S. Office of Personnel Management 2017, Federal holidays, url: https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/
+#'
 #' @name gefcom
 #' @docType data
 #' @keywords data
